6a02a07c6c3bf8c0fe76b10bc08e5f1f9fa1bf7b
Fabrizio Colonna <colofabrix@tin.it>
2019-11-28 00:12:08 +0000

    Fuffing aroudn trying to see if Shapeless can help making the parsing generic

--- accounting-service/src/main/scala/com/colofabrix/scala/accounting/banks/Halifax.scala
+++ accounting-service/src/main/scala/com/colofabrix/scala/accounting/banks/Halifax.scala
@@ -1,12 +1,15 @@
 package com.colofabrix.scala.accounting.banks
 
 import java.time.LocalDate
+import scala.runtime.Nothing$
+import cats.data.Kleisli
 import cats.implicits._
 import com.colofabrix.scala.accounting.csv.CsvDefinitions._
 import com.colofabrix.scala.accounting.csv.CsvTypeParser._
 import com.colofabrix.scala.accounting.model.HalifaxTransaction
 import shapeless._
 import shapeless.syntax.std.tuple._
+import syntax.std.traversable._
 
 
 object Halifax {
@@ -22,21 +25,33 @@ object Halifax {
 
     /** Converts a Csv row into a BankTransaction */
     def convertRow(row: CsvRow): CsvValidated[HalifaxTransaction] = {
-      val parsers = (
-        parse[LocalDate] (r => r(0))("dd/MM/yyyy"),
-        parse[LocalDate] (r => r(1))("dd/MM/yyyy"),
-        parse[String]    (r => r(2)),
-        parse[String]    (r => r(3)),
-        parse[BigDecimal](r => r(4)) map (value => -1.0 * value),
-      )
+      val parsers =
+        parse[LocalDate]  (r => r(0))("dd/MM/yyyy") ::
+        parse[LocalDate]  (r => r(1))("dd/MM/yyyy") ::
+        parse[String]     (r => r(2)) ::
+        parse[String]     (r => r(3)) ::
+        (parse[BigDecimal](r => r(4)) map (value => -1.0 * value)) ::
+        HNil
 
       object applyRow extends Poly1 {
         implicit def aDefault[A] = at[CsvRowParser[A]](f => f(row))
       }
 
-      parsers.map(applyRow).mapN(HalifaxTransaction)
+      parsers.tupled.map(applyRow).mapN(HalifaxTransaction)
     }
-  }
+
+    implicit val hnilConvert: CsvRowParser[HNil.type] = Kleisli { _ => HNil.validNec} }
+
+    implicit def hlistConvert[H, T <: HList](
+        implicit
+        hParser: Lazy[CsvRowParser[H]],
+        tParser: Lazy[CsvRowParser[T]]
+    ): CsvRowParser[H] :: T = {
+      Kleisli { rowValues => hParser.value(rowValues) }
+
+      ???
+    }
+}
 
   implicit val halifaxCsvConverter: CsvConverter[HalifaxTransaction] = HalifaxCsvFile
 
